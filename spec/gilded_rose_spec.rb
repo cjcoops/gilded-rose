91f026b394350c788a3492cd7e7b8a606b4a5114
Chris Cooper <cjcooper252@gmail.com>
2017-01-25 13:42:55 +0000

    writes tests for conjured normal items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -140,6 +140,42 @@ describe GildedRose do
 
     end
 
+    describe "conjured" do
+
+      describe "normal item" do
+
+        it "reduces quality by 2" do
+          items = [Item.new("normal", 10, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 8
+        end
+
+        it "reduces sellin by 1" do
+          items = [Item.new("normal", 10, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 9
+        end
+
+        it "reduces quality a twice as fast after sellin reaches 0" do
+          items = [Item.new("normal", 0, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 6
+        end
+
+        it "quality can not go below zero" do
+          items = [Item.new("normal", 10, 0)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 0
+        end
+
+        it "quality can not go below zero" do
+          items = [Item.new("normal", 0, 3)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 0
+        end
+      end
+    end
+
   end
 
 end
