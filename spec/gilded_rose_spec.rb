39a54c4e8aff9d1ff2ecb2d06cb0ec703125d5e0
Chris Cooper <cjcooper252@gmail.com>
2017-01-25 10:20:05 +0000

    all tests written for normal item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,6 +3,7 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
+
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
@@ -21,6 +22,18 @@ describe GildedRose do
         items = [Item.new("normal", 10, 10)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 9
+      end
+
+      it "reduces quality a twice as fast after sellin reaches 0" do
+        items = [Item.new("normal", 0, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 8
+      end
+
+      it "quality can not go below zero" do
+        items = [Item.new("normal", 10, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
       end
 
     end
