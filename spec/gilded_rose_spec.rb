70641df6baf836a8e5173949665a330ce858240d
Chris Cooper <cjcooper252@gmail.com>
2017-01-25 10:12:52 +0000

    writes test for quality and sell in on normal items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,23 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    describe "normal item" do
+
+      it "reduces quality by 1" do
+        items = [Item.new("normal", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 9
+      end
+
+      it "reduces sellin by 1" do
+        items = [Item.new("normal", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 9
+      end
+
+    end
+
   end
 
 end
