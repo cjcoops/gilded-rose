01c87a785bdef3aa0d1878afcf684a15d29ddb5a
Chris Cooper <cjcooper252@gmail.com>
2017-01-25 14:36:05 +0000

    refactors item class into own file

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,3 +1,5 @@
+require_relative './item'
+
 class GildedRose
 
   def initialize(items)
@@ -60,18 +62,4 @@ class GildedRose
     item.name.include?("Conjured")
   end
 
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
 end
